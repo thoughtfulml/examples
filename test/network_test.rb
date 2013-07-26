ed6cb547ad002f0b1b1a4c95ac352471d8edd221
Matt Kirk <meteor.kirk@gmail.com>
2013-07-26 09:17:36 -0700

    Skeleton tests for Network

---
+++ test/network_test.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+
+class NetworkTest < MiniTest::Test
+  def setup
+    @network = Network.new(26, 12, 6)
+    @fat_network = Network.new(200, 50, 50, 3)
+  end
+  
+  def test_output_layer
+    assert_equal 6, @network.outputs.length
+    assert_equal 3, @fat_network.outputs.length
+  end
+  
+  def test_input_layer
+    assert_equal 26, @network.inputs.length
+    assert_equal 200, @fat_network.inputs.length
+  end
+  
+  def test_hidden_layer_size
+    assert_equal 12, @network.hidden_neurons
+    assert_equal 100, @fat_network.hidden_neurons
+  end
+  
+  def test_to_s
+    assert_equal '<Network: 26-12-6>', @network.to_s
+    assert_equal '<Network: 200-50-50-3>', @fat_network.to_s
+  end
+end
