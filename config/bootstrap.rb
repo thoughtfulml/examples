3c53166a8738c0e9ce590b1ed21131f2511ca909
Matt Kirk <meteor.kirk@gmail.com>
2013-10-20 19:58:17 -0700

    You get out of there

--- config/bootstrap.rb
+++ config/bootstrap.rb
@@ -10,7 +10,7 @@ end
 
 require_relative '../lib/corpus'
 
-Redis.new.flushall
+REDIS.flushall
 
 Corpus.parse_file("./config/rt-polaritydata/rt-polarity.pos", 1, 5331)
 Corpus.parse_file("./config/rt-polaritydata/rt-polarity.neg", -1, 5331)
