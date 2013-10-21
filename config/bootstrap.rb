ba477cdd40f4ad97b4978963016eece365074d30
Matt Kirk <meteor.kirk@gmail.com>
2013-10-20 19:53:08 -0700

    Tweaking just a bit

--- config/bootstrap.rb
+++ config/bootstrap.rb
@@ -1,7 +1,8 @@
 require 'bundler'
 Bundler.require
-if ENV['REDISCLOUD_URL']
-  REDIS_CLOUD = URI.parse(ENV["REDISCLOUD_URL"])
+
+if ENV["REDISCLOUD_URL"]
+  uri = URI.parse(ENV["REDISCLOUD_URL"])
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 else
   REDIS = Redis.new
--- lib/corpus.rb
+++ lib/corpus.rb
@@ -2,7 +2,6 @@ require 'matrix'
 require 'libsvm'
 
 class Corpus
-  REDIS = Redis.new
   def self.parse_file(filepath, sentiment, lines)
     pbar = ProgressBar.create(:title => "#{File.basename(filepath)}", :total => lines)
 
@@ -14,30 +13,7 @@ class Corpus
     REDIS.set("sentiment_analyzer:#{File.basename(filepath)}:processed", 1)
   end
 
-  # def self.to_data(flush = true)
-  #   out = []
-  #   REDIS.flushall if flush
-  #
-  #   %w[pos neg].each do |sent|
-  #     filepath = "./config/rt-polaritydata/rt-polarity.#{sent}"
-  #     sentiment = (sent == 'pos') ? "+1" : "-1"
-  #     parse_file(filepath, sentiment, 5331) if flush
-  #
-  #     File.open(filepath, 'rb').each_line do |line|
-  #       vec = [sentiment]
-  #       vec << sparse_vector(line).sort_by(&:first).map {|kv| kv.join(":")}
-  #       out << vec.join(' ')
-  #     end
-  #   end
-  #
-  #   File.open('data.txt', 'wb') do |f|
-  #     out.each do |l|
-  #       f.write("#{l}\n")
-  #     end
-  #   end
-  #   out.length
-  # end
-
+  # Creates a vector that is just indices pointing to 1's
   def self.sparse_vector(string)
     sparse_vector = {}
     string.gsub(/[\.\?\!]/, '').downcase.split(/\s+/).each do |word|
@@ -48,38 +24,9 @@ class Corpus
     sparse_vector
   end
 
+  # Mainly wrap the sparse_vector in something
   def self.vector(string)
     Libsvm::Node.features(sparse_vector(string))
-  end
-
-  def self.validation_data
-    y_vec = []
-    x_mat = []
-    rows.times do |row_num|
-      next if row_num % 2 == 0
-      x_mat << hash_for(row_num)
-      y_vec << y_for(row_num)
-    end
-    [y_vec, x_mat]
-  end
-
-  def self.cross_validate!
-    -15.step(15,2) do |i|
-      total = 0
-      errors = 0
-      c = 2 ** i
-      model = Corpus.train_svm!(c, 2)
-      ys, xes = Corpus.validation_data
-
-      ys.each_with_index do |y, i|
-        expected = y
-        achieved = model.predict(Libsvm::Node.features(xes[i]))
-        total += 1
-        errors += 1 if achieved != expected
-      end
-
-      puts "#{errors.to_f / total} for 2 ** #{i}"
-    end
   end
 
   def self.rows
